c9c57443538ab36c12ba228d2d759e1cff60b7a6
Branimir Espinoza <brani.espinoza@gmail.com>
2023-06-27 15:34:28 -0400

    Get a single job finished

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,12 +3,20 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
-  res.status(StatusCodes.OK).json({jobs, count: jobs.length})
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-  res.send('get job')
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req
+  const job = await Job.findOne({ createdBy: userId, _id: jobId })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId} found`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
