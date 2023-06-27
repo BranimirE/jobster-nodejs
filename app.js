e8d09f8c4c39c7fa91b6b64a3b19ff3fe20315b4
Branimir Espinoza <brani.espinoza@gmail.com>
2023-06-27 19:41:28 -0400

    Adding security packages

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
-const app = express();
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const app = express()
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 // connect DB
 const connectDB = require('./db/connect')
@@ -11,30 +15,40 @@ const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.use(express.json());
 // extra packages
+app.set('trust proxy', 1) //Set this prop when we are behind a reverse proxy
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100
+  })
+)
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
