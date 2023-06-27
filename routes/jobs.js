011cd1e20ef8ecb0fed8da0159600505989b429b
Branimir Espinoza <brani.espinoza@gmail.com>
2023-06-26 22:55:24 -0400

    Adding authentication for /jobs routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,5 +1,11 @@
 const express = require('express')
-const { createJob, getAllJobs, getJob, deleteJob, updateJob } = require('../controllers/jobs')
+const {
+  createJob,
+  getAllJobs,
+  getJob,
+  deleteJob,
+  updateJob,
+} = require('../controllers/jobs')
 const router = express.Router()
 
 router.route('/').post(createJob).get(getAllJobs)
