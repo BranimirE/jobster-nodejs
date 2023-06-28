ddd2d5734420abb38ce294698465edbf143b0a4c
Branimir Espinoza <brani.espinoza@gmail.com>
2023-06-27 23:48:28 -0400

    Adding Swagger documentation

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config()
 require('express-async-errors')
-const express = require('express')
-const app = express()
+
+// extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
+
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+const express = require('express')
+const app = express()
 
 // connect DB
 const connectDB = require('./db/connect')
@@ -18,7 +26,7 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-// extra packages
+// security middlewares
 app.set('trust proxy', 1) //Set this prop when we are behind a reverse proxy
 app.use(
   rateLimiter({
@@ -30,6 +38,11 @@ app.use(express.json())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
