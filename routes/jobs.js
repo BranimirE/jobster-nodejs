9e43c7b13826f498a0b938ae9f33018b15f816df
Branimir Espinoza <brani.espinoza@gmail.com>
2023-07-25 19:10:30 -0400

    Implementing the dashboard endpoint

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,11 +5,13 @@ const {
   getJob,
   deleteJob,
   updateJob,
+  showStats,
 } = require('../controllers/jobs')
 const testUser = require('../middleware/test-user')
 const router = express.Router()
 
 router.route('/').post(testUser, createJob).get(getAllJobs)
+router.route('/stats').get(showStats)
 router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob)
 
 module.exports = router
