ccee1ff470f3e62f43b473c329d1845d1d8378ff
Branimir Espinoza <brani.espinoza@gmail.com>
2023-08-16 00:50:33 -0400

    Testing the auth PATCH endpoint

--- tests/routes/auth.test.js
+++ tests/routes/auth.test.js
@@ -64,7 +64,43 @@ describe('Auth routes', () => {
     await User.findOneAndRemove({ email: user.email })
   })
 
-  it('PATCH /api/v1/auth/updateUser', () => {
+  it('PATCH /api/v1/auth/updateUser', async () => {
+    const userData = {
+      name: 'TestUserName',
+      email: 'testuser@testemail.com',
+      password: 'secret',
+    }
+    const updateData = {
+      name: 'TestUserNameUpdated',
+      email: 'testuserupdate@testemail.com',
+      lastName: 'lastNameValue',
+      location: 'New location'
+    }
+    // Remove the user if it already exists and the one that we are going to update for
+    await User.findOneAndRemove({ email: userData.email })
+    // We cannot update the email if it alredy exist
+    await User.findOneAndRemove({ email: updateData.email })
+    let user = await User.create(userData)
+    const token = user.createJWT()
 
+    const response = await request(app).patch('/api/v1/auth/updateUser').set('Authorization', `Bearer ${token}`).send(updateData)
+
+    // It should respond the updated data
+    expect(response.status).toBe(200)
+    expect(response.body.user.name).toBe(updateData.name)
+    expect(response.body.user.email).toBe(updateData.email)
+    expect(response.body.user.lastName).toBe(updateData.lastName)
+    expect(response.body.user.location).toBe(updateData.location)
+    expect(response.body.user.token).toBeTruthy()
+
+    // Check database data
+    user = await User.findOne({_id: user._id})
+    expect(user.name).toBe(updateData.name)
+    expect(user.email).toBe(updateData.email)
+    expect(user.lastName).toBe(updateData.lastName)
+    expect(user.location).toBe(updateData.location)
+
+    // Remove the upadted user
+    await user.delete()
   })
 })
